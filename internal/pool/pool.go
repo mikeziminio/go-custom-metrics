013c8ba780b22f9f25abeadc4583d120c13b6547
Mike Zimin <mikeziminio@gmail.com>
2026-03-28 07:38:44 +0300

    add mutex to pool

--- internal/pool/pool.go
+++ internal/pool/pool.go
@@ -1,26 +1,32 @@
 package pool
 
+import "sync"
+
 type Resettable interface {
 	Reset()
 }
 
 type Pool[T Resettable] struct {
-	pool *[]T
+	pool []T
 	new  func() T
+	mu   sync.Mutex
 }
 
 func New[T Resettable](newFunc func() T) *Pool[T] {
 	return &Pool[T]{
-		pool: &[]T{},
+		pool: []T{},
 		new:  newFunc,
 	}
 }
 
 func (p *Pool[T]) Get() T {
-	if len(*p.pool) > 0 {
-		last := len(*p.pool) - 1
-		obj := (*p.pool)[last]
-		*p.pool = (*p.pool)[:last]
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	if len(p.pool) > 0 {
+		last := len(p.pool) - 1
+		obj := p.pool[last]
+		p.pool = p.pool[:last]
 		return obj
 	}
 	return p.new()
@@ -28,5 +34,7 @@ func (p *Pool[T]) Get() T {
 
 func (p *Pool[T]) Put(obj T) {
 	obj.Reset()
-	*p.pool = append(*p.pool, obj)
+	p.mu.Lock()
+	p.pool = append(p.pool, obj)
+	p.mu.Unlock()
 }
